bc5bd48bffe5d54697989a5095456aed3dca5743
Markus Lampert <markuslampert@usv-w126.fh-brandenburg.de>
2018-01-16 08:29:25 +0100

    Please enter the commit message for your changes. Lines starting sdfjöl

--- movie-recommend/src/main/java/org/camunda/bpm/movie/movierecommend/MovieRecommendApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.movie.movierecommend;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Movie Recommend App")
-public class MovieRecommendApplication extends ServletProcessApplication {
-	  // empty implementation
-}
