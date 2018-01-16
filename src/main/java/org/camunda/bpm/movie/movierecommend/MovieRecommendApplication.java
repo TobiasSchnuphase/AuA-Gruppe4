44e668ae4b674c86538fa924652450056e45daa4
markuslampert <32674352+markuslampert@users.noreply.github.com>
2018-01-16 08:34:29 +0100

    Add files via upload

---
+++ src/main/java/org/camunda/bpm/movie/movierecommend/MovieRecommendApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.movie.movierecommend;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Movie Recommend App")
+public class MovieRecommendApplication extends ServletProcessApplication {
+	  // empty implementation
+}
